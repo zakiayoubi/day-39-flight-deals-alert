9d2c007099be67c8dbd4afc06ba76882448a72a9
ayoubim <ayoubim@berea.edu>
2023-10-25 12:33:48 -0400

    Initial commit

---
+++ notification_manager.py
@@ -0,0 +1,15 @@
+# class NotificationManager:
+#     #This class is responsible for sending notifications with the deal flight details.
+#     pass
+
+
+
+
+myList = [{1, 2, 3}, {"a", "b"}, {"a", 1, 2}]
+
+for i in myList:
+    print(i)
+
+
+for i in range(len(myList)):
+    print(i)
